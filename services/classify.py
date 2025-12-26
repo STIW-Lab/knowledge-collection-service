63cdb0da267db4bc363f87b99b227af7757e1981
akshatsabavat <akshat.sabavat@gmail.com>
2025-12-26 16:45:47 -0500

    Created: Singleton class for classify operations

---
+++ services/classify.py
@@ -0,0 +1,11 @@
+class Classify:
+    __instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            # New instance if class does not exist
+            cls.__instance = super(Classify, cls).__new__(cls, *args, **kwargs)
+            
+        # Return class each time
+        return cls.__instance
+    def __init__(self):
+        pass
