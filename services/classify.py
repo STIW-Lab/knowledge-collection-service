320e4ba2b91fa86c372c773bb27e8d539a84d58e
akshatsabavat <akshat.sabavat@gmail.com>
2025-12-26 16:48:54 -0500

    Chore: Added packages for transformers & torch

--- services/classify.py
+++ services/classify.py
@@ -1,11 +1,16 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+# Single ton class to classify 
 class Classify:
     __instance = None
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
             # New instance if class does not exist
             cls.__instance = super(Classify, cls).__new__(cls, *args, **kwargs)
-            
         # Return class each time
         return cls.__instance
+    
     def __init__(self):
         pass
